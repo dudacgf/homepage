5353404f73b5fadd6c5a621c147671b0beb51298
dudacgf <dudacgf.cadastro@gmail.com>
2024-06-28 13:20:15 -0300

    initial upload

---
+++ admin/db_backup.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once('../common.php');
+
+// localização do xml com detalhes da conexão e o número da conexão a ser utilizada...
+$connection_info_xml_path = $config_path . 'connections.xml';
+$connection_info_xml_id = 1;
+$connections = simplexml_load_file($connection_info_xml_path);
+
+foreach ($connections as $connection)
+{
+    if ($connection['ID'] == $connection_info_xml_id)
+    {
+        $dbHost = (string) $connection->dbHost;
+        $dbUser = (string) $connection->dbUser;
+        $dbPassword = (string) $connection->dbPassword;
+        $dbSchema  = (string) $connection->dbSchema;
+    }
+}
+
+$filename = 'homepage-' . date('Ymd') . '.bkp.sql';
+$fileWithPath = HOMEPAGE_PATH . '/backup/' . $filename;
+$mysqldumpCmd = 'mysqldump --user ' . $dbUser . ' -p\'' . $dbPassword . '\' -h ' . $dbHost . ' -i -A --add-drop-table --add-drop-trigger -r ' . $fileWithPath;
+
+$output = exec($mysqldumpCmd);
+
+if (php_sapi_name() != "cli") {
+    header('Content-Description: File Transfer');
+    header('Content-Disposition: attachment; filename='.$filename);
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($fileWithPath));
+    header("Content-Type: text/plain");
+    readfile($fileWithPath);
+    echo('<script type="text/javascript">window.history.back();</script>');
+}
+
+?>
