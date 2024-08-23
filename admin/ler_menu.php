2135a5daab97ec1f68a353ae990f3840d6b0cb88
dudacgf <dudacgf.cadastro@gmail.com>
2024-08-23 15:57:46 -0300

    refactor na leitura dos itens do menu

--- admin/ler_menu.php
+++ admin/ler_menu.php
@@ -1,68 +1,44 @@
 <?php
+require_once('../common.php');
 $admPag = new pagina(ID_ADM_PAG);
 
 //
 // Leio todos os elementos da página e percorro-os, quebrando por categoria e grupo e os vou incluindo no template
-$menusLidos = $admPag->getBigArray();
-$categAnterior = 'xx';
-$grupoAnterior = 'xx';
+$admPag->lerElementos();
+foreach ($admPag->elementos as $categ) {
 
-foreach ($menusLidos as $menu) {
+    $menuCategorias[] = array('index' => $categ->posPagina, 'categoria' => $categ->descricaoCategoria);
+    
+    // Leio os grupos desta categoria e percorro-os, incluíndo-os no template
+    $categ->lerElementos();
+    $tempGrupos = [];
+    foreach ($categ->elementos as $umGrupo) 
+    {
 
-    // realiza a quebra por grupo. 
-    // para cada grupo, tenho que guardar sua descricao para comparar novamente e seu tipo para o template.
-    // para o primeiro grupo da primeira categoria, como obviamente ainda não tenho elementos, faço uma quebra falsa.
-    if ( $menu['descricaoGrupo'] != $grupoAnterior ) {
-        if ( isset ( $menus ) ) {
-            $grupos[] = array('grupo' => $grupoAnterior, 'idtipoGrupo' => $idTipoGrupoAnterior, 'elementos' => $menus);
-            unset ( $menus );
-        }
-        $grupoAnterior = $menu['descricaoGrupo'];
-        $idTipoGrupoAnterior = $menu['idTipoGrupo'];
+        // Leio os elementos deste grupo e percorro-os, incluíndo-os no template
+        $umGrupo->lerElementos();
+        $menutItems = [];
+        foreach($umGrupo->elementos as $menuItem) 
+        {
+            $menuItems[] = $menuItem->getArray();
+        }       
+
+        $tempGrupos[] = array(
+                        'grupo' => $umGrupo->descricaoGrupo,
+                        'idtipoGrupo' => $umGrupo->idTipoGrupo,
+                        'menuItems' => $menuItems);
+
     }
+    
+    $menuGrupos[] = array(
+                            'index' => $umGrupo->posCategoria, 
+                            'idGrupo' => $umGrupo->idGrupo,
+                            'grupos' => $tempGrupos
+                            );
 
-    // realiza a quebra por categoria.
-    // para cada categoria, tenho que guardar sua descricao para comparar novamente e sua posicao na pagina.
-    // para a primeira categoria, como obviamente não tenho grupos, faço uma quebra falsa.
-    if ( $menu['descricaoCategoria'] != $categAnterior ) {
-        if ( isset ( $grupos ) ) {
-            $menuGrupos[] = array('index' => $posicaoAnterior, 'idGrupo' => $menu['idGrupo'], 'grupos' => $grupos);
-            unset($grupos);
-        }
-        $categAnterior = $menu['descricaoCategoria'];
-        $posicaoAnterior = $menu['posPagina'];
-        $menuCategorias[] = array('index' => $menu['posPagina'], 'categoria' => $menu['descricaoCategoria']);
-    }
-
-    // incluo os elementos de um grupo num array que na quebra de grupos será adicionado a um array de grupos.
-    // este array de grupos será incluido num array co-irmão do array de categorias a cada quebra de categoria.
-    $menus[] = array( 
-                   'idElemento' => $menu['idElemento'],
-                   'descricaoLink' => $menu['descricaoElemento'],
-                   'tipoElemento' => $menu['idTipoElemento'],
-                   'posGrupo' => $menu['posGrupo'], 
-                   'linkURL' => $menu['urlElemento'],
-                   'localLink' => $menu['urlElementoLocal'], 
-                   'dicaLink' => $menu['dicaElemento'], 
-                   'targetLink' => $menu['urlElementoTarget'], 
-                   'nomeForm' => $menu['formNome'], 
-                   'descricaoForm' => $menu['descricaoElemento'],
-                   'acao' => $menu['urlElemento'],
-                   'nomeCampo' => $menu['formNomeCampo'],
-                   'tamanhoCampo' => $menu['formTamanhoCampo'], 
-                   'breakBefore' => $menu['separadorBreakBefore'], 
-                   'descricaoSeparador' => $menu['descricaoElemento'],
-                   'urlImagem' => $menu['urlElemento'],
-                   'descricaoImagem' => $menu['descricaoElemento'],
-                   'nomeTemplate' => $menu['templateFileName'],
-                   'urlElementoSSL' => $menu['urlElementoSSL'], 
-                   'urlElementoSVN' => $menu['urlElementoSVN']
-            );
 }
 
 // neste tipo de loop com quebra no início, fica sempre faltando adicionar os grupos da última categoria.
-$grupos[] = array('grupo' => $grupoAnterior, 'idtipoGrupo' => $idTipoGrupoAnterior, 'elementos' => $menus);
-$menuGrupos[] = array('index' => $posicaoAnterior, 'idGrupo' => $menu['idGrupo'], 'grupos' => $grupos);
 $homepage->assign('menuCategorias', $menuCategorias);
 $homepage->assign('menuGrupos', $menuGrupos);
 ?>
