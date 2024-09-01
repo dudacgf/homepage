9d048ccc34b67d893c57c4d67a69eb231069f4e0
dudacgf <dudacgf.cadastro@gmail.com>
2024-09-01 16:28:15 -0300

    simplificacao e padronizacao do codigo com utilizacao de chamadaAPI

---
+++ js/chamadaAPI.js
@@ -0,0 +1,27 @@
+/**
+ * Executa uma chamada API via fetch assíncrono
+ *
+ * @param {string} metodo - o metodo api a ser executado
+ * @param {json} options - dict json com opções para submissão (body, method etc)
+ *
+ * @returns {json} - o json retornado pela execução do método action
+ */
+async function chamadaAPI(metodo, options = {}) {
+    const url = window.includePATH + 'api/' + metodo + '.php';
+
+    try {
+        const response = await fetch(url, options);
+        
+        if (!response.ok) 
+            return JSON.parse(`{"status": "error", "message": "${response.status}"}`);
+
+        let responseData = await response.json();
+        let r = eval("(" + responseData + ")");
+        /*let r = JSON.parse(responseData);*/
+        return r;
+
+    } catch(err) {
+        return JSON.parse(`{"status": "error", "message": "${err.message}"}`);
+    };
+};
+
